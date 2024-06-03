a0d7ff2434c36ec1330b2bfabc447e4ef61b2c5a
EltonArmelini <eltonarmelini.it@gmail.com>
2024-06-03 07:18:54 -0300

    Clase: 03/06

---
+++ modulos/parte-ii-control/estructuras-control.js
@@ -0,0 +1,60 @@
+/*
+
+Operadores logicos: 
+AND (&&):
+condición1 && condición2
+
+OR (||):
+condición1 || condición2
+
+NOT(!):
+!condición
+
+*/
+
+/*
+Operadores de Comparación
+
+Igualdad (==):
+valor1 == valor2
+
+Estrictamente igual (===):
+valor1 === valor2
+
+Desigualdad (!=):
+valor1 != valor2
+
+Desigualdad estricta (!==):
+valor1 !== valor2
+
+Mayor, Menor, Mayor Igual y Menor Igual:
+valor1 > valor2
+valor1 < valor2
+valor1 >= valor2
+valor1 <= valor2
+*/
+
+
+/* Sentencia If() */
+if (condición) {
+  // código a ejecutar si la condición es verdadera
+} else {
+  // código a ejecutar si la condición es falsa
+}
+
+
+/* Sentencia Switch() */
+switch (expresión) {
+  case valor1:
+    // código a ejecutar si expresión es igual a valor1
+    break;
+  case valor2:
+    // código a ejecutar si expresión es igual a valor2
+    break;
+  default:
+    // código a ejecutar si ninguno de los casos anteriores es verdadero
+    break;
+}
+
+/* Operador ternario */
+condición ? expresión_si_verdadera : expresión_si_falsa;
